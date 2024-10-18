def6946d9a85f57edd62670539a707518ed3be9a
Jan Quesada <janjosueq@gmail.com>
2024-10-17 21:23:27 -0600

    Semana5

---
+++ PROYECTO_SC_504/src/main/java/com/mycompany/proyecto_sc_504/PROYECTO_SC_504.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_sc_504;
+
+/**
+ *
+ * @author quesa
+ */
+public class PROYECTO_SC_504 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
